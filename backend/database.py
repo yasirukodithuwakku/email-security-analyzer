62e0567b0e201154f0abb845c394459f5946f997
Yasiru Kodithuwakku <yasirukodithuwakku2000@gmail.com>
2026-04-12 08:41:49 +0530

    Update Email Header Analyzer

--- backend/database.py
+++ backend/database.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./scans.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
